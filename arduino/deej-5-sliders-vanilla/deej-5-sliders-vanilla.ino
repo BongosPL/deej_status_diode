a0b8c9c6b7c82b083292f4cbd101d87bf8521122
BongosPL <Kamilosos2004@gmail.com>
2024-05-23 13:48:41 +0200

    Update deej-5-sliders-vanilla.ino
    - Startup diodes animation
    - Status LED diodes

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,5 +1,10 @@
 const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+const int analogInputs[NUM_SLIDERS] = {A0, A2, A3, A4, A6};
+
+//delays section
+const int del = 50;
+const int del2 = 390;
+
 
 int analogSliderValues[NUM_SLIDERS];
 
@@ -8,7 +13,73 @@ void setup() {
     pinMode(analogInputs[i], INPUT);
   }
 
+  pinMode(8, INPUT_PULLUP);
+  
+  pinMode(13, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(8, OUTPUT);
+
   Serial.begin(9600);
+// Start test animation
+// 1st
+digitalWrite(13, HIGH);
+digitalWrite(2, HIGH);
+delay(del);
+digitalWrite(13, LOW);
+digitalWrite(4, HIGH);
+delay(del);
+digitalWrite(2, LOW);
+digitalWrite(6, HIGH);
+delay(del);
+digitalWrite(4, LOW);
+digitalWrite(8, HIGH);
+delay(del);
+digitalWrite(6, LOW);  
+delay(del);
+digitalWrite(8, LOW);
+delay(del);
+digitalWrite(13, HIGH);
+digitalWrite(2, HIGH);
+delay(del);
+digitalWrite(13, LOW);
+digitalWrite(4, HIGH);
+delay(del);
+digitalWrite(2, LOW);
+digitalWrite(6, HIGH);
+delay(del);
+digitalWrite(4, LOW);
+digitalWrite(8, HIGH);
+delay(del);
+digitalWrite(6, LOW);  
+delay(del);
+digitalWrite(8, LOW);
+delay(500);
+///2nd
+digitalWrite(13, HIGH);
+digitalWrite(2, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(8, HIGH);
+delay(700);
+digitalWrite(13, LOW);
+digitalWrite(2, LOW);
+digitalWrite(4, LOW);
+digitalWrite(6, LOW);
+digitalWrite(8, LOW);
+delay(700);
+digitalWrite(13, HIGH);
+digitalWrite(2, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(8, HIGH);
+delay(700);
+digitalWrite(13, LOW);
+digitalWrite(2, LOW);
+digitalWrite(4, LOW);
+digitalWrite(6, LOW);
+digitalWrite(8, LOW);
 }
 
 void loop() {
@@ -16,6 +87,41 @@ void loop() {
   sendSliderValues(); // Actually send data (all the time)
   // printSliderValues(); // For debug
   delay(10);
+
+
+  if(analogRead(0) > 4 && analogRead(6) > 4){          //// LED ON if...
+    digitalWrite(13, HIGH);
+  }
+  else {
+  digitalWrite(13, LOW);
+  }
+
+  if(analogRead(2) > 4 && analogRead(6) > 4){          //// LED ON if...
+    digitalWrite(2, HIGH);
+  }
+  else {
+    digitalWrite(2, LOW);
+  }
+  if(analogRead(3) > 4 && analogRead(6) > 4){          //// LED ON if...
+    digitalWrite(4, HIGH);
+  }
+  else {
+  digitalWrite(4, LOW);
+  }
+
+  if(analogRead(4) > 4 && analogRead(6) > 4){          //// LED ON if...
+    digitalWrite(6, HIGH);
+  }
+  else {
+    digitalWrite(6, LOW);
+  }
+  if(analogRead(6) > 4 && analogRead(6) > 4){          //// LED ON if...
+    digitalWrite(8, HIGH);
+  }
+  else {
+    digitalWrite(8, LOW);
+  }
+
 }
 
 void updateSliderValues() {
